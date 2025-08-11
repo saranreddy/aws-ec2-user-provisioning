aeff676e26122460c3d904d74d2437c53fce7b86
saran reddysaran.reddy2002@gmail.com <you@example.com>
2025-08-11 11:35:26 -0500

    Fix outputs.tf: Remove S3 bucket references to resolve Terraform validation errors - Replace S3-dependent outputs with SSH key status information - Update next steps to reflect current workflow approach - Ensure Terraform configuration is fully valid

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,30 +21,25 @@ output "instances_provisioned" {
   value       = var.instance_ids
 }
 
-output "user_s3_key_locations" {
-  description = "S3 locations of SSH keys for each user (to be downloaded for emailing)"
+output "ssh_key_status" {
+  description = "Status of SSH key installation for each user"
   value = {
     for username, user in yamldecode(data.local_file.users_config.content).users : username => {
-      private_key_s3_path = "s3://${data.aws_s3_bucket.ssh_keys.bucket}/keys/${username}_private_key"
-      public_key_s3_path  = "s3://${data.aws_s3_bucket.ssh_keys.bucket}/keys/${username}_public_key"
-      email               = user.email
-      full_name           = user.full_name
+      username     = user.username
+      email        = user.email
+      full_name    = user.full_name
+      ssh_key_installed = contains(keys(var.user_public_keys), username)
     }
   }
-}
-
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket storing SSH keys"
-  value       = data.aws_s3_bucket.ssh_keys.bucket
 }
 
 output "next_steps" {
   description = "Next steps after provisioning"
   value = [
-    "1. SSH keys have been generated and stored securely in S3 bucket: ${data.aws_s3_bucket.ssh_keys.bucket}",
+    "1. SSH keys have been generated and installed on EC2 instances",
     "2. Users have been created on all specified EC2 instances",
-    "3. Public keys from S3 have been added to each user's authorized_keys",
-    "4. Private keys will be downloaded from S3 and emailed to users automatically",
-    "5. Users can SSH to instances using their private keys downloaded from email"
+    "3. Public keys have been added to each user's authorized_keys",
+    "4. Users can SSH to instances using their private keys",
+    "5. SSH keys are available in the workflow for distribution"
   ]
 } 
